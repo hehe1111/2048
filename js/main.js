0010d3a2e95d033ae8eb3fba648d6182d8bebba2
hehe1111 <2908749709@qq.com>
2018-07-15 21:19:22 +0800

    【新增】游戏逻辑。

--- js/main.js
+++ js/main.js
@@ -22,3 +22,37 @@ function init() {
         }
     }
 }
+
+for (let i = 0; i < 4; i++) {
+    board[i] = []
+    for (let j = 0; j < 4; j++) {
+        board[i][j] = 0
+    }
+}
+
+updateBoardView()
+
+function updateBoardView() {
+    $('.number-cell').remove()
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            $('.grid-container').append(`<div class="number-cell" id="number-cell-${i}-${j}"></div>`)
+            let numberCell = $('.number-cell-${i}-${j}')
+
+            if (board[i][j] === 0) {
+                numberCell.css('width', '0px')
+                numberCell.css('height', '0px')
+                numberCell.css('top', getPositionTop(i) + 50)
+                numberCell.css('left', getPositionLeft(j) + 50)
+            } else {
+                numberCell.css('width', '100px')
+                numberCell.css('height', '100px')
+                numberCell.css('top', getPositionTop(i))
+                numberCell.css('left', getPositionLeft(j))
+                numberCell.css('background-color', getNumberBackgroundColor(board[i][j]))
+                numberCell.css('color', getNumberColor(board[i][j]))
+                numberCell.text(board[i][j])
+            }
+        }
+    }
+}
--- js/support.js
+++ js/support.js
@@ -5,3 +5,27 @@ function getPositionTop(i) {
 function getPositionLeft(j) {
     return 20 + j * 120
 }
+
+function getNumberBackgroundColor(number) {
+    switch (number) {
+        case 2: return '#eee4da'; break;
+        case 4: return '#ede0c8'; break;
+        case 8: return '#f2b179'; break;
+        case 16: return '#f59563'; break;
+        case 32: return '#f67c5f'; break;
+        case 64: return '#f65e3b'; break;
+        case 128: return '#edcf72'; break;
+        case 256: return '#edcc61'; break;
+        case 512: return '#99cc00'; break;
+        case 1024: return '#33b5e5'; break;
+        case 2048: return '#0099cc'; break;
+        case 4096: return '#aa66cc'; break;
+        case 8192: return '#9933cc'; break;
+        default: return '#000000'; break;
+    }
+}
+
+function getNumberColor(number) {
+    if (number <= 4) { return '#776e65' }
+    return '#ffffff'
+}
